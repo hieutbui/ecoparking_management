ce601bf538c683dd487d74b8aa8838b550ba5759
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-12-08 20:40:49 +0700

    temp

---
+++ lib/utils/custom_logger.dart
@@ -0,0 +1,25 @@
+import 'package:loggy/loggy.dart';
+
+mixin ControllerLoggy implements LoggyType {
+  @override
+  Loggy<ControllerLoggy> get loggy =>
+      Loggy<ControllerLoggy>('Controller Loggy - $runtimeType');
+}
+
+mixin GetItLoggy implements LoggyType {
+  @override
+  Loggy<GetItLoggy> get loggy =>
+      Loggy<GetItLoggy>('GetIt Loggy - $runtimeType');
+}
+
+mixin InteractorLoggy implements LoggyType {
+  @override
+  Loggy<InteractorLoggy> get loggy =>
+      Loggy<InteractorLoggy>('Interactor Loggy - $runtimeType');
+}
+
+mixin ServiceLoggy implements LoggyType {
+  @override
+  Loggy<ServiceLoggy> get loggy =>
+      Loggy<ServiceLoggy>('Service Loggy - $runtimeType');
+}
