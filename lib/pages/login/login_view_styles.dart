efd434397b795c879f6ff1f8ecf0c1bb5e9f717b
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-12-09 22:42:39 +0700

    Separate login styles

---
+++ lib/pages/login/login_view_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+class LoginViewStyles {
+  static const double spacing = 16.0;
+
+  static const EdgeInsetsGeometry padding =
+      EdgeInsets.symmetric(horizontal: 16.0);
+}
