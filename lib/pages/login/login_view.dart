2a7ec142ff2dfc83bc21a88e0a3255afa04d9900
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-12-09 22:42:39 +0700

    Separate login styles

--- lib/pages/login/login_view.dart
+++ lib/pages/login/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:ecoparking_management/config/app_paths.dart';
 import 'package:ecoparking_management/domain/state/account/sign_in_state.dart';
 import 'package:ecoparking_management/pages/login/login.dart';
+import 'package:ecoparking_management/pages/login/login_view_styles.dart';
 import 'package:ecoparking_management/utils/mixins/custom_logger.dart';
 import 'package:ecoparking_management/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
@@ -21,12 +22,12 @@ class LoginView extends StatelessWidget with ViewLoggy {
         physics: const ClampingScrollPhysics(),
         child: Column(
           children: <Widget>[
-            const SizedBox(height: 16.0),
+            const SizedBox(height: LoginViewStyles.spacing),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: LoginViewStyles.padding,
               child: Column(
                 children: <Widget>[
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: LoginViewStyles.spacing),
                   ValueListenableBuilder(
                     valueListenable: controller.signInNotifier,
                     builder: (context, state, child) {
@@ -40,7 +41,7 @@ class LoginView extends StatelessWidget with ViewLoggy {
                       );
                     },
                   ),
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: LoginViewStyles.spacing),
                   ValueListenableBuilder(
                     valueListenable: controller.signInNotifier,
                     builder: (context, state, child) {
@@ -55,7 +56,7 @@ class LoginView extends StatelessWidget with ViewLoggy {
                       );
                     },
                   ),
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: LoginViewStyles.spacing),
                   ValueListenableBuilder(
                     valueListenable: controller.signInNotifier,
                     builder: (context, state, child) {
