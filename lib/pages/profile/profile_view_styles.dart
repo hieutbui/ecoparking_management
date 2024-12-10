8eb3b1d22595d1e415571dcbd5343afcaa644579
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-12-10 22:40:21 +0700

    EM-04: Add styles for Profile screen

--- lib/pages/profile/profile_view_styles.dart
+++ lib/pages/profile/profile_view_styles.dart
@@ -1,1 +1,15 @@
-class ProfileViewStyles {}
+import 'package:flutter/material.dart';
+
+class ProfileViewStyles {
+  static const double spacing = 16.0;
+  static const double avatarSize = 132.0;
+  static const double dummyAvatarWidth = 95.0;
+  static const double dummyAvatarHeight = 100.0;
+  static const double avatarBottomSpacing = 24.0;
+  static const double infoLineSpacing = 8.0;
+
+  static const EdgeInsetsGeometry padding =
+      EdgeInsets.symmetric(horizontal: 16.0);
+  static const EdgeInsetsGeometry cardPadding =
+      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0);
+}
