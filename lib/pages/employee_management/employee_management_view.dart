a77735e64930ac2a115fd4876c17571a3ab42416
Hieu Bui <hieu.bui@base.vn>
2024-12-12 13:48:57 +0700

    EF-11: Remove unused import

--- lib/pages/employee_management/employee_management_view.dart
+++ lib/pages/employee_management/employee_management_view.dart
@@ -1,14 +1,9 @@
-import 'package:ecoparking_management/config/app_config.dart';
 import 'package:ecoparking_management/config/app_paths.dart';
 import 'package:ecoparking_management/pages/employee_management/employee_management.dart';
-import 'package:ecoparking_management/pages/employee_management/employee_management_view_styles.dart';
 import 'package:ecoparking_management/pages/employee_management/widgets/employee_view.dart';
 import 'package:ecoparking_management/pages/employee_management/widgets/owner_view.dart';
-import 'package:ecoparking_management/pages/live_overview/widgets/info_multi_columns.dart';
-import 'package:ecoparking_management/pages/live_overview/widgets/table_info.dart';
 import 'package:ecoparking_management/utils/mixins/custom_logger.dart';
 import 'package:ecoparking_management/widgets/app_scaffold.dart';
-import 'package:ecoparking_management/widgets/info_card_with_title.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeManagementView extends StatelessWidget with ViewLoggy {
