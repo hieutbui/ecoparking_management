928c539a24c22a89c07697da0a465d5aa1325f30
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-12-29 17:14:03 +0700

    EM-26: Not allow Previous screen on mobile

--- lib/pages/previous_analysis/previous_analysis_view.dart
+++ lib/pages/previous_analysis/previous_analysis_view.dart
@@ -7,6 +7,7 @@ import 'package:ecoparking_management/pages/previous_analysis/widgets/chart_card
 import 'package:ecoparking_management/pages/previous_analysis/widgets/highlighted_child/revenue_highlighted_child.dart';
 import 'package:ecoparking_management/pages/previous_analysis/widgets/highlighted_child/vehicle_highlighted_child.dart';
 import 'package:ecoparking_management/utils/mixins/custom_logger.dart';
+import 'package:ecoparking_management/utils/platform_infos.dart';
 import 'package:ecoparking_management/widgets/app_scaffold.dart';
 import 'package:ecoparking_management/widgets/dropdown_previous_view_type/dropdown_previous_view_type.dart';
 import 'package:flutter/material.dart';
@@ -30,81 +31,91 @@ class PreviousAnalysisView extends StatelessWidget with ViewLoggy {
         ),
         onPressed: controller.openScanner,
       ),
-      body: SingleChildScrollView(
-        physics: const ClampingScrollPhysics(),
-        child: Padding(
-          padding: PreviousAnalysisViewStyles.padding,
-          child: Column(
-            children: <Widget>[
-              InfoMultiColumns(
-                columns: <ColumnArguments>[
-                  ColumnArguments(
-                    highlightedChild:
-                        RevenueHighlightedChild(controller: controller),
-                    secondaryChild: Row(
-                      children: <Widget>[
-                        Text(
-                          'Total Revenue ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineLarge!
-                              .copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .surfaceContainerHighest,
+      body: PlatformInfos.isMobile
+          ? Center(
+              child: Text(
+                'This page is not available on mobile',
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      color:
+                          Theme.of(context).colorScheme.surfaceContainerHighest,
+                    ),
+              ),
+            )
+          : SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: Padding(
+                padding: PreviousAnalysisViewStyles.padding,
+                child: Column(
+                  children: <Widget>[
+                    InfoMultiColumns(
+                      columns: <ColumnArguments>[
+                        ColumnArguments(
+                          highlightedChild:
+                              RevenueHighlightedChild(controller: controller),
+                          secondaryChild: Row(
+                            children: <Widget>[
+                              Text(
+                                'Total Revenue ',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineLarge!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .surfaceContainerHighest,
+                                    ),
                               ),
+                              DropdownPreviousViewType(
+                                initialViewType:
+                                    controller.viewTypeTotalRevenue.value,
+                                onSelectViewType:
+                                    controller.onSelectViewTypeTotalRevenue,
+                              ),
+                            ],
+                          ),
                         ),
-                        DropdownPreviousViewType(
-                          initialViewType:
-                              controller.viewTypeTotalRevenue.value,
-                          onSelectViewType:
-                              controller.onSelectViewTypeTotalRevenue,
+                        ColumnArguments(
+                          highlightedChild:
+                              VehicleHighlightedChild(controller: controller),
+                          secondaryChild: Row(
+                            children: <Widget>[
+                              Text(
+                                'Total Vehicle ',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineLarge!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .surfaceContainerHighest,
+                                    ),
+                              ),
+                              DropdownPreviousViewType(
+                                initialViewType:
+                                    controller.viewTypeTotalVehicleCount.value,
+                                onSelectViewType: controller
+                                    .onSelectViewTypeTotalVehicleCount,
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  ColumnArguments(
-                    highlightedChild:
-                        VehicleHighlightedChild(controller: controller),
-                    secondaryChild: Row(
-                      children: <Widget>[
-                        Text(
-                          'Total Vehicle ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineLarge!
-                              .copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .surfaceContainerHighest,
-                              ),
-                        ),
-                        DropdownPreviousViewType(
-                          initialViewType:
-                              controller.viewTypeTotalVehicleCount.value,
-                          onSelectViewType:
-                              controller.onSelectViewTypeTotalVehicleCount,
-                        ),
-                      ],
+                    const SizedBox(
+                      height: PreviousAnalysisViewStyles.spacing,
                     ),
-                  ),
-                ],
+                    RevenueChartCard(controller: controller),
+                    const SizedBox(
+                      height: PreviousAnalysisViewStyles.spacing,
+                    ),
+                    VehicleCountChartCard(controller: controller),
+                    const SizedBox(
+                      height: PreviousAnalysisViewStyles.spacing,
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: PreviousAnalysisViewStyles.spacing,
-              ),
-              RevenueChartCard(controller: controller),
-              const SizedBox(
-                height: PreviousAnalysisViewStyles.spacing,
-              ),
-              VehicleCountChartCard(controller: controller),
-              const SizedBox(
-                height: PreviousAnalysisViewStyles.spacing,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
